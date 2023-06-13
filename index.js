42222cc5ce787bc56c974177348689216d031ab6
Ray <rainiery10457@gmail.com>
2023-06-13 11:51:58 -0400

    fixed js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let totalAmount = document.getElementById("total-amount");
 let userAmount = document.getElementById("user-amount");
 const checkAmountButton = document.getElementById("check-amount");
-const totalAmountButton =  document.getElementById("total-amount-button");
+const totalAmountButton = document.getElementById("total-amount-button");
 const productTitle = document.getElementById("product-title");
 const errorMessage = document.getElementById("budget-error");
 const productTitleError = document.getElementById("product-title-error");
@@ -12,31 +12,39 @@ const balanceValue = document.getElementById("balance-amount");
 const list = document.getElementById("list");
 let tempAmount = 0;
 
+// Set Budget Functions
+
 totalAmountButton.addEventListener("click", () => {
     tempAmount = totalAmount.value;
+    // Bad input
     if (tempAmount === "" || tempAmount < 0) {
         errorMessage.classList.remove("hide");
- } else {
-    errorMessage.classList.remove("hide");
-    amount.innerHTML = tempAmount;
-    balanceValue.innerText = tempAmount - expenditureValue.innerText;
-    totalAmount.value = "";
- }
+    } else {
+        errorMessage.classList.add("hide");
+        // Set bidget
+        amount.innerHTML = tempAmount;
+        balanceValue.innerText = tempAmount - expenditureValue.innerText;
+        // Clear input
+        totalAmount.value = "";
+    }
 });
+
+// Disable edit and delete button function
 
 const disableButtons = (bool) => {
     let editButtons = document.getElementsByClassName("edit");
     Array.from(editButtons).forEach((element) => {
-        element.disable = bool;
+        element.disabled = bool;
     });
-
 };
 
+// Modify list elements function
+
 const modifyElement = (element, edit = false) => {
-    let parentDiv= element.parentElement;
+    let parentDiv = element.parentElement;
     let currentBalance = balanceValue.innerText;
     let currentExpense = expenditureValue.innerText;
-    let parentAmount = parentDiv.querySelector(".product").innerText;
+    let parentAmount = parentDiv.querySelector(".amount").innerText;
     if (edit) {
         let parentText = parentDiv.querySelector(".product").innerText;
         productTitle.value = parentText;
@@ -44,12 +52,12 @@ const modifyElement = (element, edit = false) => {
         disableButtons(true);
     }
 
-
-balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
-expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount);
-parentDiv.remove();
-
+    balanceValue.innerText = parseInt(currentBalance) + parseInt(parentAmount);
+    expenditureValue.innerText = parseInt(currentExpense) - parseInt(parentAmount);
+    parentDiv.remove();
 };
+
+// Create list function
 
 const listCreator = (expenseName, expenseValue) => {
     let subListContent = document.createElement("div");
@@ -59,7 +67,7 @@ const listCreator = (expenseName, expenseValue) => {
     let editButton = document.createElement("button");
     editButton.classList.add("fa-solid", "fa-pen-to-square", "edit");
     editButton.style.fontSize = "1.2em";
-    editButton.addEventListener("click", () =>  {
+    editButton.addEventListener("click", () => {
         modifyElement(editButton, true);
     });
     let deleteButton = document.createElement("button");
@@ -73,19 +81,27 @@ const listCreator = (expenseName, expenseValue) => {
     document.getElementById("list").appendChild(subListContent);
 };
 
-checkAmountButton.addEventListener("click", () => {
-    if (!userAmount.value || !productTitle.value) {
-    productTitleError.classList.remove("hide");
-    return false;
-}
-disableButtons(false);
-let expenditure = parseInt(userAmount.value);
-let sum = parseInt(expenditureValue.innerText) + expenditure;
-expenditureValue.innerText = sum;
-const totalBalance = tempAmount - sum;
-balanceValue.innerText = totalBalance;
-listCreator(productTitle.value, userAmount);
-productTitle.value = "";
-userAmount.value = "";
+// Add expenses function
 
+checkAmountButton.addEventListener("click", () => {
+    // Check empty
+    if (!userAmount.value || !productTitle.value) {
+        productTitleError.classList.remove("hide");
+        return false;
+    }
+    // Enable buttons
+    disableButtons(false);
+    //Expense
+    let expenditure = parseInt(userAmount.value);
+    // Total expense (existing + new)
+    let sum = parseInt(expenditureValue.innerText) + expenditure;
+    expenditureValue.innerText = sum;
+    // Total balance = budget - total expense
+    const totalBalance = tempAmount - sum;
+    balanceValue.innerText = totalBalance;
+    //Create list
+    listCreator(productTitle.value, userAmount.value);
+    //Clear inputs
+    productTitle.value = "";
+    userAmount.value = "";
 });
