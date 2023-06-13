b4aae02b399f29ea7066d26d2ab69195aebc86c1
Ray <rainiery10457@gmail.com>
2023-06-13 09:18:02 -0400

    start js, finished css

---
+++ index.js
@@ -0,0 +1,4 @@
+let totalAmount = document.getElementById("total-amount");
+let userAmount = document.getElementById("user-amount");
+const checkAmountButton = document.getElementById("check-amount");
+const totalAmountButton =  document.getElementById("total-amount-button");
